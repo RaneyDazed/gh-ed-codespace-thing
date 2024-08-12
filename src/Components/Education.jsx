7cc5bac444b69b095775eedd051cfe4f0c413b4e
CHAIR <95461636+RaneyDazed@users.noreply.github.com>
2024-08-12 16:49:43 +0000

    chore: Update Education component ID to match casing in JSX file

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,16 +1,11 @@
 import React from "react"; // Import the React library
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faGraduationCap } from "@fortawesome/free-solid-svg-icons"; // Import the faGraduationCap icon
 
 const Education = () => {
   return (
-    <section id="education">
+    <section id="Education">
       <div className="container">
         <div className="education">
-          <h2>
-            <FontAwesomeIcon icon={faGraduationCap} />
-            Education
-          </h2>
+          <h2>Education</h2>
           <p>
             This is a great area to highlight your educational background. You can include your
             school, area of study, degree obtained, and any honors or awards you may have received.
